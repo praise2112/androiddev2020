8efcf878f7bc2991aa59b5d53f7fed066fb33eaa
Praise2112 <oketola.praise@gmail.com>
2019-11-16 20:12:55 +0700

    Labwork 5

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -1,20 +1,42 @@
 package vn.edu.usth.weather;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import java.util.Random;
 
 
 public class ForecastFragment extends Fragment {
+    private String[] days, description;
+    private int[] images = {R.drawable.clear, R.drawable.cloudy, R.drawable.thundershowers, R.drawable.light_rain, R.drawable.thunderstorms, R.drawable.hot, R.drawable.foggy};
+    private Random rand = new Random();
+//  return day iteratively from the days array
+    public String getNextDay(int j){
+        if(j >= 7) {   // keeps value of i between array length
+            j = j%7;
+        }
+        return days[j];
+    }
+//    return random description
+    public String getRandDescription(int j){
+        if(j >= 7) {   // keeps value of i between array length
+            j = j%7;
+        }
+        return description[j];
+    }
 
     public ForecastFragment( ) {
         // Required empty public constructor
@@ -31,7 +53,32 @@ public class ForecastFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_forecast, container, false);
+        View view = inflater.inflate(R.layout.forecast_fragment, container, false);
+
+        days = getResources().getStringArray(R.array.days);
+        description = getResources().getStringArray(R.array.weather_description);
+
+        LinearLayout fragment_container = (LinearLayout) view.findViewById(R.id
+                .fragment_container);
+        LinearLayout scroll_view_container = (LinearLayout) view.findViewById(R.id
+                .scroll_view_container);
+
+
+//        Generating multiple forecast_fragment_item and adding them into fragment_container
+        for (int i = 0; i < 16 ; i++) {
+//            modifying a forecast_fragment_item
+            final View v = inflater.inflate(R.layout.forecast_fragment_item, null);
+            TextView day = (TextView) v.findViewById(R.id.day);
+            TextView weatherDescription = (TextView) v.findViewById(R.id.weatherDescription);
+            ImageView weatherImg = (ImageView) v.findViewById(R.id.weatherImg);
+            day.setText(getNextDay(i));
+            weatherDescription.setText(getRandDescription(i));
+            weatherImg.setImageResource(images[rand.nextInt(images.length)]);   // picking random image from array
+            scroll_view_container.addView(v);   // adding forest_item_fragment to scroll view
+        }
+
+//        inflater.inflate(R.layout.forecast_fragment_item, fragment_container);
+        return view;
     }
 }
 
